5b218eecb4596ae3ada3c1e558d99a42eec5ec26
EnGud <60407025+EnGud@users.noreply.github.com>
2020-03-23 17:06:51 +0100

    Working class/children, randomly generated & case

--- ESP32-Firebase.ino
+++ ESP32-Firebase.ino
@@ -1,6 +1,7 @@
+#include <stdio.h>
 #include <WiFi.h>
 #include <FirebaseESP32.h>
-#include <time.h>
+// + #include <time.h>
 
 FirebaseData firebaseData;
 FirebaseJson json;
@@ -12,16 +13,22 @@ FirebaseJson json;
 #define RXD2 16
 #define TXD2 17
 
-String path = "/Test";
+//Declarations
 int Info = 20;
 String Dataplassering;
-char Inndata;
+char Inndata[32];
+char Destination[16];
+
+
+
+
+
 
 void setup() {
 
 
   Serial.begin(115200);
-  
+  Serial.println("");
   Serial.print("Kobler til nettverk");
   
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD); //input SSID og passord
@@ -31,14 +38,13 @@ while(WiFi.status() != WL_CONNECTED){
   Serial.print(".");
   delay(300);
 }
-
-
-Serial.println("Tilkobling vellykket, ");
+// + Timeout ved for lang tid, "Prøv igjen"
+Serial.println("");
+Serial.print("Tilkobling vellykket, ");
 Serial.print("Koblet til IP: ");
 Serial.println(WiFi.localIP());
-Serial.println();
-
-Serial.println();
+Serial.println("");
+delay(100);
 Serial.print("Kobler til Firebase.");
 
 Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
@@ -51,33 +57,34 @@ Firebase.setMaxErrorQueue(firebaseData, 10);
  
 // Firebase.enableClassicRequest(firebaseData, true);
 // Benyttes dersom vi trenger get og delete-funksjoner senere (Mest sannsynlig ikke).
-//setDeviceTime() henter tid fra serveren. bruke RTC, eller hente tid hver gang man sender?
-  Serial.begin(115200);
-  //Serial1.begin(9600, SERIAL_8N1, RXD2, TXD2);
-  Serial2.begin(9600, SERIAL_8N1, RXD2, TXD2);
+// + setDeviceTime() henter tid fra serveren. bruke RTC, eller hente tid hver gang man sender?
+
+  Serial2.begin(115200, SERIAL_8N1, RXD2, TXD2);
   Serial.println("Serial Txd is on pin: "+String(TX));
   Serial.println("Serial Rxd is on pin: "+String(RX));
 
-if (Firebase.pushInt(firebaseData, path + "/Arnes", Info))
-{
-  Serial.println("Data sent");
-  Dataplassering = firebaseData.dataPath();
-  Serial.println(Dataplassering);
-  delay(10*100);}
+//if (Firebase.pushInt(firebaseData, path, Info))
+//{
+//  Serial.println("Data sent");
+//  Dataplassering = firebaseData.dataPath();
+//  Serial.println(Dataplassering);
+//  delay(10*100);}
 }
 
 
 
 void loop(){
 int Info = random(10, 35);
-String path = "/Test";
+RandTestFull();
+String path = Destination;
+int Inndata = rand()%30 +1;
 
   while (Serial2.available()) {
     Serial.print(char(Serial2.read()));
 Inndata = Serial2.read();
   }
   
-if (Firebase.setInt(firebaseData, path + "/Arnes", Inndata))
+if (Firebase.setInt(firebaseData, path, Inndata))
 {
   Serial.println("Data sent");
   delay(5000);
@@ -90,13 +97,24 @@ else
 }
 
 }
-
-
-
  
-
-
-
+void RandTestFull(){
+int Num = rand()%3;
+  delay(1000);
+  switch(Num){
+    case  0:
+    strcpy(Destination, "1pck5CC382xF");
+    break;
+    
+    case 1:
+    strcpy(Destination, "2pKK4mEn8Dx0");
+    break;
+    
+    case 2:
+    strcpy(Destination, "TestSetting");
+    break;
+}
+}
 //Manglende kode/Idéer
 //Sleep-status
 //Wait For Recieved bit
