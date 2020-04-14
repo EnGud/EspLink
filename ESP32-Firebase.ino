bb83e0a9afa9011ef69e582d7f8717ffe9a14720
EnGud <60407025+EnGud@users.noreply.github.com>
2020-04-14 15:54:36 +0200

    Update ESP32-Firebase.ino

--- ESP32-Firebase.ino
+++ ESP32-Firebase.ino
@@ -1,7 +1,9 @@
+#include <Arduino.h>
 #include <stdio.h>
-#include <WiFi.h>
+#include <string>
 #include <FirebaseESP32.h>
-// + #include <time.h>
+#include <wifi.h>
+
 
 FirebaseData firebaseData;
 FirebaseJson json;
@@ -10,112 +12,136 @@ FirebaseJson json;
 #define WIFI_PASSWORD "djea2642"
 #define FIREBASE_HOST "frbasewebapprlcrowdsolutions20.firebaseio.com"
 #define FIREBASE_AUTH "ia4dsTciYNUb6ghqrVkvMhJBf9yJK70B7mwMVK9R"
-#define RXD2 16
-#define TXD2 17
 
-//Declarations
-int Info = 20;
-String Dataplassering;
-char Inndata[32];
-char Destination[16];
+//Declarations//
+char Indata[27];
+char NodeID[] = "Teststring";
 
+char *p = Indata;
 
+char NodeTemp[32];
+char NodeDoor[32];
+char NodeLight[32];
+char NodeBattery[32];
+char NodeNoise[32];
 
+// int Temperature;
+// int DoorState;
+// int Light;
+// int Battery;
+// int Noise;
+int DataInfo[] = {00, 0, 0, 00, 00};
 
+int i, j;
 
+bool DataAvailable = false;
+const int Index = 25;
+char Temp[Index];
+
+//End declarations//
 
 void setup() {
 
-
-  Serial.begin(115200);
+  Serial.begin(9600);
   Serial.println("");
-  Serial.print("Kobler til nettverk");
-  
+  Serial.print("Connecting to Wi-Fi");
+
+  //Connect wifi while not connected.
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD); //input SSID og passord
   Firebase.reconnectWiFi(true);
 
-while(WiFi.status() != WL_CONNECTED){
-  Serial.print(".");
-  delay(300);
-}
-// + Timeout ved for lang tid, "Prøv igjen"
-Serial.println("");
-Serial.print("Tilkobling vellykket, ");
-Serial.print("Koblet til IP: ");
-Serial.println(WiFi.localIP());
-Serial.println("");
-delay(100);
-Serial.print("Kobler til Firebase.");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(300);
+  }
 
-Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
-Firebase.reconnectWiFi(true);
+  // + Timeout ved for lang tid, "Prøv igjen"
+  //Output success, testing purposes.
+
+  Serial.println("");
+  Serial.print("Connection established, ");
+  Serial.print("Connected to IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println("");
+  delay(100);
+  Serial.println("Connecting to Firebase.");
+
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.reconnectWiFi(true);
   // Firebase Innlogging, etterfulgt av antal forsøk ved error, og maks error i queue.
 
-Firebase.setMaxRetry(firebaseData, 3);
-Firebase.setMaxErrorQueue(firebaseData, 10);
+  Firebase.setMaxRetry(firebaseData, 3);
+  Firebase.setMaxErrorQueue(firebaseData, 10);
 
- 
-// Firebase.enableClassicRequest(firebaseData, true);
-// Benyttes dersom vi trenger get og delete-funksjoner senere (Mest sannsynlig ikke).
-// + setDeviceTime() henter tid fra serveren. bruke RTC, eller hente tid hver gang man sender?
 
-  Serial2.begin(115200, SERIAL_8N1, RXD2, TXD2);
-  Serial.println("Serial Txd is on pin: "+String(TX));
-  Serial.println("Serial Rxd is on pin: "+String(RX));
+  // Firebase.enableClassicRequest(firebaseData, true);
+  // Benyttes dersom vi trenger get og delete-funksjoner senere (Mest sannsynlig ikke).
+  // + setDeviceTime() henter tid fra serveren. bruke RTC, eller hente tid hver gang man sender?
 
-//if (Firebase.pushInt(firebaseData, path, Info))
-//{
-//  Serial.println("Data sent");
-//  Dataplassering = firebaseData.dataPath();
-//  Serial.println(Dataplassering);
-//  delay(10*100);}
+  Serial2.begin(9600);
+  Serial.println("Serial2 Initiated");
 }
 
-
-
-void loop(){
-int Info = random(10, 35);
-RandTestFull();
-String path = Destination;
-int Inndata = rand()%30 +1;
-
-  while (Serial2.available()) {
-    Serial.print(char(Serial2.read()));
-Inndata = Serial2.read();
+void CheckData(){
+  if (Serial2.available()){
+    DataAvailable = true;
+  }
+  else
+  {
+    DataAvailable = false;
   }
   
-if (Firebase.setInt(firebaseData, path, Inndata))
-{
-  Serial.println("Data sent");
-  delay(5000);
-}
-else
-{
-  Serial.println("Feilet!");
-  Serial.println("Feilkode: " + firebaseData.errorReason());
-  delay(5000);
 }
 
+void GetData(){
+
+  
+  if (Serial2.available() > 0){
+
+    Serial2.readBytesUntil('@', Temp, Index);
+    strcpy(Indata, Temp);
+  }
+  Serial.println(Indata);
+
 }
- 
-void RandTestFull(){
-int Num = rand()%3;
-  delay(1000);
-  switch(Num){
-    case  0:
-    strcpy(Destination, "1pck5CC382xF");
-    break;
+
+
+
+//Funksjon for å splitte innkommende data i rette grupper, for å så skrive rett Firebase-addresse for infoen.
+void SplitData(){
+char * token = strtok(Indata, ":");
+strcpy(NodeID, token);
+
+for (i=0; i<=4; i++){
+token = strtok(NULL, ":");
+DataInfo[i] = atoi(token);
+}
+
+  strcpy(NodeTemp, strcat(NodeID, "/Temp")); 
+  strcpy(NodeDoor, strcat(NodeID, "/Door"));
+  strcpy(NodeLight, strcat(NodeID, "/Light"));
+  strcpy(NodeBattery, strcat(NodeID, "/Battery"));
+  strcpy(NodeNoise, strcat(NodeID, "/Noise"));
+}
+
+void loop(){
+  CheckData();
+if (DataAvailable == true){
+  GetData();
+
+}
+SplitData();
+
+for (i=0; i<=4; i++){
+    if (Firebase.setInt(firebaseData, NodeTemp, DataInfo[i])) {
+      Serial.println("Temp sent");
+}
+else {
+           Serial.println("Feilet!");
+        Serial.println("Feilkode: " + firebaseData.errorReason());
+        delay(50);
     
-    case 1:
-    strcpy(Destination, "2pKK4mEn8Dx0");
-    break;
-    
-    case 2:
-    strcpy(Destination, "TestSetting");
-    break;
+}
+DataAvailable = false;
 }
 }
-//Manglende kode/Idéer
-//Sleep-status
-//Wait For Recieved bit
-//Noden må sende dataen, samt en unik ID til CC, ID-en benyttes for å lagre dataen i rett database i Firebase.
